b7fbe235ffa86d5707f95381571496eb4bc65c8b
binaryfr3ak <binaryfr3ak@gmail.com>
2016-02-05 08:45:25 +0100

    Add support for queud builds

--- G19BuildScreen/G19BuildScreen/G19BuildScreenApplet.xaml.cs
+++ G19BuildScreen/G19BuildScreen/G19BuildScreenApplet.xaml.cs
@@ -9,6 +9,10 @@ using TeamProject = Microsoft.TeamFoundation.Framework.Client.Catalog.Objects.Te
 namespace G19BuildScreen
 {
     using System.Configuration;
+    using System.Windows.Media;
+    using System.Windows.Threading;
+    using Brush = System.Windows.Media.Brush;
+    using Color = System.Drawing.Color;
 
     /// <summary>
     ///     Interaction logic for G19BuildScreenApplet.xaml
@@ -18,6 +22,7 @@ namespace G19BuildScreen
         string tfsUsername;
         string tfsPassword;
         string tfsUri;
+        DispatcherTimer timer;
         public G19BuildScreenApplet()
         {
             InitializeComponent();
@@ -29,27 +34,77 @@ namespace G19BuildScreen
             tfsPassword = config.AppSettings.Settings["Password"].Value;
 
             tfsUri = config.AppSettings.Settings["Uri"].Value;
-            //// this.GetBuilds();
+            
+            timer = new DispatcherTimer(TimeSpan.FromSeconds(5.0), DispatcherPriority.Render, this.UpdateUserInterface, 
+                Dispatcher.CurrentDispatcher);
+            this.GetBuilds();
+        }
+
+        private void UpdateUserInterface(object sender, EventArgs eventArgs)
+        {
+            this.GetBuilds();
         }
 
         public void GetBuilds()
         {
-            NetworkCredential cred = new NetworkCredential("user", "pass");
+            NetworkCredential cred = new NetworkCredential(this.tfsUsername, this.tfsPassword);
             
-            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(new Uri("uri"), cred);
+            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(new Uri(this.tfsUri), cred);
 
             tfs.Authenticate();
             var buildService = (IBuildServer)tfs.GetService(typeof(IBuildServer));
             {
                 if (buildService != null)
                 {
-                    IBuildDefinition buildDetailSpec = buildService.GetBuildDefinition("project", "Definition");
+                    IBuildDefinition buildDetailSpec = buildService.GetBuildDefinition("DvdManager", "DvdManager_CI_Main");
 
-                    IBuildDetail build = buildService.GetBuild(buildDetailSpec.LastBuildUri);
+                    IQueuedBuildSpec queed = buildService.CreateBuildQueueSpec("DvdManager", "DvdManager_CI_Main");
 
-                    if (build.Status == BuildStatus.Succeeded)
+                    IQueuedBuildQueryResult queryResult = buildService.QueryQueuedBuilds(queed);
+
+                    if (queryResult.QueuedBuilds.Length > 0)
                     {
-                        Console.WriteLine("Successful");
+                        foreach (IQueuedBuild queuedBuild in queryResult.QueuedBuilds)
+                        {
+                            switch (queuedBuild.Status)
+                            {
+                                case QueueStatus.InProgress:
+                                    this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.Black);
+                                    break;
+                                case QueueStatus.Canceled:
+                                    this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.Coral);
+                                    break;
+                                default:
+                                    this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.Fuchsia);
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                        IBuildDetail build = buildService.GetBuild(buildDetailSpec.LastBuildUri);
+
+                        switch (build.Status)
+                        {
+                            case BuildStatus.Succeeded:
+                                this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.Green);
+                                break;
+                            case BuildStatus.Failed:
+                                this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.Red);
+                                break;
+                            case BuildStatus.InProgress:
+                                this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.Black);
+                                break;
+                            case BuildStatus.PartiallySucceeded:
+                                this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.Orange);
+                                break;
+                            case BuildStatus.Stopped:
+                                this.StatusBorder.BorderBrush = new SolidColorBrush(Colors.PaleVioletRed);
+                                break;
+                            default:
+                                break;
+                        }
                     }
                 }
             }
